4e205ca0194f8f8be60d149a3af6dbe8dd5bb6b7
Kalpana Gurnani <97270196+kalpanagurnani11@users.noreply.github.com>
2026-03-07 15:15:34 +0530

    Update ptr_in_function.cpp

--- cpp/assignment1/ptr_in_function.cpp
+++ cpp/assignment1/ptr_in_function.cpp
@@ -1,13 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//void display(int *p)
+//{
+//	cout<<"value is:"<<*p<<endl;
+//	cout<<"address of value is:"<<p<<endl;
+//}
+//int main()
+//{
+//	int n=10;
+//	display(&n);
+//}
+
 #include<iostream>
 using namespace std;
 
-void display(int *p)
+void add(int *a, int *b)
 {
-	cout<<"value is:"<<*p<<endl;
-	cout<<"address of value is:"<<p<<endl;
+    int sum;
+    sum = *a + *b;
+    cout<<"Sum = "<<sum;
 }
+
 int main()
 {
-	int n=10;
-	display(&n);
+    int x = 10, y = 20;
+
+    add(&x,&y);
+
+    return 0;
 }
